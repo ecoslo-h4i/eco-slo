731047ca9deb28343edab9af873c635482769424
David Benshachar <dbenshachar@gmail.com>
2026-01-16 17:54:26 -0800

    feat: add function to create authorized user  in supabase-client.ts and implement POST function for trees table.

--- src/app/api/admin/trees/route.ts
+++ src/app/api/admin/trees/route.ts
@@ -1,4 +1,5 @@
-import { supabase } from "@/supabase-client";
+import { TablesInsert } from "@/database/database.types";
+import { createAuthenticatedClient } from "@/supabase-client";
 import { NextRequest, NextResponse } from "next/server";
 
 /**
@@ -9,10 +10,19 @@ export async function GET() {
   return NextResponse.json({ message: "Example trees GET message" });
 }
 
-/**
- * Example POST API route. REPLACE THIS DOCSTRING.
- * @returns {message: string}
- */
-export async function POST() {
-  return NextResponse.json({ message: "Example trees POST message" });
+export async function POST(request: NextRequest) {
+  try {
+    const bearerToken = request.headers.get("Authorization") ?? "";
+    const authSupabase = createAuthenticatedClient(bearerToken);
+    const json = await request.json();
+
+    const body = json as TablesInsert<"trees">;
+    const response = await authSupabase.from("trees").insert(body).select();
+    return NextResponse.json({ message: response.data, info: response.error?.message }, { status: response.status });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: null, info: error.message }, { status: 500 });
+    }
+  }
+  return NextResponse.json({ message: null, info: null }, { status: 500 });
 }
--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,6 +1,16 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const createAuthenticatedClient = (jwt: string): SupabaseClient => {
+  return createClient(supabaseUrl, supabaseKey, {
+    global: {
+      headers: {
+        Authorization: `${jwt}`,
+      },
+    },
+  });
+};
