455aec7501c1084556f6d2a0b4fe245278c1f4da
Oliver-Cushman <olivermcushman@gmail.com>
2026-01-18 14:29:50 -0800

    fix: ensure consistent ordering

--- src/app/api/admin/trees/route.ts
+++ src/app/api/admin/trees/route.ts
@@ -7,7 +7,7 @@ import { NextRequest, NextResponse } from "next/server";
  * @returns { message: string, status: number } if error
  */
 export async function GET() {
-  const { data, error } = await supabase.from("trees").select();
+  const { data, error } = await supabase.from("trees").select("*").order("created_at", { ascending: true });
   if (error) {
     return NextResponse.json({ message: error.message }, { status: 404 });
   }
