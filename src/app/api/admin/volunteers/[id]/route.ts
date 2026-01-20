93b8086cff2c834f8dd3cfd27fba122c412f0869
Noah <noahschn317@gmail.com>
2026-01-20 15:50:06 -0800

    feat: create preliminary code for volunteers/[id] PUT

--- src/app/api/admin/volunteers/[id]/route.ts
+++ src/app/api/admin/volunteers/[id]/route.ts
@@ -1,6 +1,10 @@
 import { supabase } from "@/supabase-client";
 import { NextRequest, NextResponse } from "next/server";
 
+type Iparams = {
+  id: string;
+};
+
 /**
  * Example GET API route. REPLACE THIS DOCSTRING.
  * @returns {message: string}
@@ -13,8 +17,16 @@ export async function GET() {
  * Example PUT API route. REPLACE THIS DOCSTRING.
  * @returns {message: string}
  */
-export async function PUT() {
-  return NextResponse.json({ message: "Example volunteers slug PUT message" });
+export async function PUT(request: NextRequest, params: Iparams) {
+  const id = params.id;
+  const body = await request.json();
+  const message = await supabase.from("volunteers").update(body).eq("id", id).select().single();
+
+  if (message.error) {
+    return NextResponse.json({ error: "Error" }, { status: 404 });
+  }
+
+  return NextResponse.json({ message: message }, { status: 200 });
 }
 
 /**
