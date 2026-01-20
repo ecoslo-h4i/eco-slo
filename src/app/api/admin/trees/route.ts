036ebbaf16a7b114be536ee46fbc10628723b17b
David Benshachar <dbenshachar@gmail.com>
2026-01-20 08:26:54 -0800

    fix: add more robust error handling for the POST method in trees.

--- src/app/api/admin/trees/route.ts
+++ src/app/api/admin/trees/route.ts
@@ -1,5 +1,6 @@
 import { TablesInsert } from "@/database/database.types";
-import { createAuthenticatedClient } from "@/supabase-client";
+import { postgrestErrorToHttpStatus } from "@/database/utils";
+import { createAuthenticatedClient, supabase } from "@/supabase-client";
 import { NextRequest, NextResponse } from "next/server";
 
 /**
@@ -14,25 +15,28 @@ export async function POST(request: NextRequest) {
   try {
     const bearerToken = request.headers.get("Authorization") ?? "";
     if (!bearerToken) {
-      return NextResponse.json(
-        { message: null, info: "Bearer token is missing in headers for POST request." },
-        { status: 401 },
-      );
+      return NextResponse.json({ message: "Bearer token is missing in headers for POST request." }, { status: 401 });
     }
     const authSupabase = createAuthenticatedClient(bearerToken);
     const claims = await authSupabase.auth.getClaims(bearerToken);
     if (claims.data != null) {
-      return NextResponse.json({ message: null, info: "Current session is not authenticated." }, { status: 401 });
+      return NextResponse.json({ message: "Current session is not authenticated." }, { status: 401 });
     }
     const json = await request.json();
 
     const body = json as TablesInsert<"trees">;
     const response = await authSupabase.from("trees").insert(body).select();
-    return NextResponse.json({ message: response.data, info: response.error?.message }, { status: response.status });
+    if (response.error) {
+      return NextResponse.json(
+        { message: response.error.message },
+        { status: postgrestErrorToHttpStatus(response.error) },
+      );
+    }
+    return NextResponse.json({ message: response.data }, { status: 200 });
   } catch (error: unknown) {
     if (error instanceof Error) {
-      return NextResponse.json({ message: null, info: error.message }, { status: 500 });
+      return NextResponse.json({ message: error.message }, { status: 500 });
     }
+    return NextResponse.json({ message: "Unexpected server error" }, { status: 500 });
   }
-  return NextResponse.json({ message: null, info: null }, { status: 500 });
 }
