e19f92cea35457ad96f5d652bdebbcc6a32ee207
RudyG252 <Rudyagood@gmail.com>
2026-01-21 14:55:34 -0800

    feat: add slug get request for admin tree

--- src/app/api/admin/trees/[id]/route.ts
+++ src/app/api/admin/trees/[id]/route.ts
@@ -9,11 +9,23 @@ type IParams = {
 };
 
 /**
- * Example GET API route. REPLACE THIS DOCSTRING.
- * @returns {message: string}
+ * Returns a single tree row by id
+ * @returns { body: Tree[], status: number } if successful
+ * @returns { message: string, status: number} if error
  */
-export async function GET() {
-  return NextResponse.json({ message: "Example trees slug GET message" });
+export async function GET(req: NextRequest, { params }: IParams) {
+  const { id } = await params;
+
+  try {
+    const message = await supabase.from("trees").select().eq("id", id);
+    if (message.error) {
+      return NextResponse.json({ error: message.error }, { status: postgrestErrorToHttpStatus(message.error) });
+    }
+
+    return NextResponse.json({ message: message.data }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Unexpected Server Error" });
+  }
 }
 
 /**
