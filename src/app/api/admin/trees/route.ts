acaee8d3bb16771c8e4a48da673f59d8192262f6
Oliver-Cushman <olivermcushman@gmail.com>
2026-01-21 14:19:11 -0800

    fix: add postgrestErrorToHttpStatus to get trees

--- src/app/api/admin/trees/route.ts
+++ src/app/api/admin/trees/route.ts
@@ -1,18 +1,19 @@
 import { supabase } from "@/supabase-client";
 import { NextRequest, NextResponse } from "next/server";
+import { postgrestErrorToHttpStatus } from "@/database/utils";
 
 /**
  * Get all rows and columns of tree table as array of JS objects
- * @returns { body: Tree[], status: number } if successful
+ * @returns { message: Tree[], status: number } if successful
  * @returns { message: string, status: number } if error
  */
 export async function GET() {
   try {
     const { data, error } = await supabase.from("trees").select("*").order("created_at", { ascending: true });
     if (error) {
-      return NextResponse.json({ message: error.message }, { status: 404 });
+      return NextResponse.json({ message: error.message }, { status: postgrestErrorToHttpStatus(error) });
     }
-    return NextResponse.json(data, { status: 200 });
+    return NextResponse.json({ message: data }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "Unexpected server error" }, { status: 500 });
   }
