740a7fa44c9ff4a1be39401352b566554ed1211b
Matthew Blam <110935636+MatthewBlam@users.noreply.github.com>
2026-01-23 02:36:21 -0800

    Revert "feat: add GET API method with error handling."

--- src/app/api/admin/volunteers/[id]/route.ts
+++ src/app/api/admin/volunteers/[id]/route.ts
@@ -1,42 +1,12 @@
-import { postgrestErrorToHttpStatus } from "@/database/utils";
 import { supabase } from "@/supabase-client";
 import { NextRequest, NextResponse } from "next/server";
-import { Tables } from "@/database/database.types";
-
-type IParams = {
-  params: {
-    id: string;
-  };
-};
 
 /**
- * Admin GET API route for all volunteer information of specified ID.
- * @param {request: NextRequest, params: IParams}
- * @returns {message: string, status: number}
+ * Example GET API route. REPLACE THIS DOCSTRING.
+ * @returns {message: string}
  */
-export async function GET(request: NextRequest, { params }: IParams) {
-  try {
-    const { id } = await params;
-    const response = await supabase
-      .from("volunteers")
-      .select("*")
-      .eq("id", id)
-      .order("created_at", { ascending: true });
-
-    if (response.error) {
-      return NextResponse.json(
-        { message: response.error.message },
-        { status: postgrestErrorToHttpStatus(response.error) },
-      );
-    }
-
-    return NextResponse.json({ message: response.data }, { status: response.status });
-  } catch (error: any) {
-    if (error instanceof Error) {
-      return NextResponse.json({ message: error.message }, { status: 500 });
-    }
-    return NextResponse.json({ message: "Unexpected server error" }, { status: 500 });
-  }
+export async function GET() {
+  return NextResponse.json({ message: "Example volunteers slug GET message" });
 }
 
 /**
