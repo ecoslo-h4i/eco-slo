e2b83abb6826323a049091ddb40ce289dabbca3a
Noah <noahschn317@gmail.com>
2026-01-31 16:32:45 -0800

    feat: make divs for dashboard page.tsx

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+export default function Dash() {
+  return (
+    <div className="dashLayout">
+      <div className="nav"></div>
+      <div className="dashHeader">
+        <header className="dashHeaderText">Welcome Back, User</header>
+        <div className="bell"></div>
+        <div className="notepad"></div>
+      </div>
+      <div className="notiications"></div>
+      <div className="volunteers"></div>
+      <div className="reminders"></div>
+      <div className="trees"></div>
+    </div>
+  );
+}
