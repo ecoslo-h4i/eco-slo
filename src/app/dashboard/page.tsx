866e42483f898391041718f1b61e278a7bf554cb
Noah <noahschn317@gmail.com>
2026-02-04 10:43:35 -0800

    feat: add 4 container placeholders

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,17 @@
 export default function Dash() {
   return (
+    //the whole page div//
     <div className="flex min-h-screen bg-[#FBF7EE]">
+      {/*navbar*/}
       <aside className="w-[140px] bg-[#758656] shrink0"></aside>
+      {/*main*/}
       <main className="flex-1 p-7">
+        {/*header*/}
         <header className="flex items-center justify-between pt-5">
           <h1 className="flex-1 text-[56px] font-[Constantia] font-semibold leading-none">Welcome Back, User</h1>
+          {/*icons*/}
           <div className="flex items-center gap-4">
+            {/*bell*/}
             <button
               type="button"
               className={`w-[81px] h-[81px] rounded-full flex items-center justify-center 
@@ -16,22 +22,32 @@ export default function Dash() {
             >
               <img src="/assets/icons/bell.svg" alt="" />
             </button>
+            {/*notes*/}
             <button
               type="button"
               className={`w-[81px] h-[81px] rounded-full flex items-center justify-center 
                     bg-[#758656] 
                     transition-all duration-200 ease-out 
                     hover:bg-[#6A7B4F] cursor-pointer`}
-              aria-label="Tasks"
+              aria-label="Notes"
             >
               <img src="/assets/icons/notepad.svg" alt="" />
             </button>
           </div>
         </header>
-        <div className="notifications"></div>
-        <div className="volunteers"></div>
-        <div className="reminders"></div>
-        <div className="trees"></div>
+        {/*widgets*/}
+        <div className="px-6 pt-10">
+          <div className="grid grid-cols-[2fr_1fr_1fr] gap-8">
+            {/*notifications*/}
+            <div className="h-[366px] rounded-3xl border-2 border-[#CDAA7F] bg-[#EEE0CF]"></div>
+            {/*volunteers*/}
+            <div className="h-[366px] rounded-3xl border-2 border-[#CDAA7F] bg-[#EEE0CF]"></div>
+            {/*reminders*/}
+            <div className="h-[366px] rounded-3xl border-2 border-[#CDAA7F] bg-[#EEE0CF]"></div>
+          </div>
+          {/*trees*/}
+          <div className="mt-8 rounded-3xl border-2 border-[#CDAA7F] h-[400px] bg-[#EEE0CF]"></div>
+        </div>
       </main>
     </div>
   );
