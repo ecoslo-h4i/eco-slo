41acaa43db9f04cfc283d61f9d03fbc589d3d5d8
Oliver-Cushman <olivermcushman@gmail.com>
2026-02-11 14:48:24 -0800

    add avenir

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,17 +6,19 @@ export default function Navbar() {
     <div className="flex flex-col min-h-screen w-[140px] bg-[#758656] pt-[48px] gap-[60px] h-[846px]">
       <div className="flex flex-col items-center justify-center gap-[20px]">
         <div className="w-[98px] h-[98px] flex items-center justify-center">
-          <Image src="/ecoslo-logo.png" width={98} height={98} alt="EcoSLO Logo" />
+          <Image src="/icons/ecoslo-logo.png" width={98} height={98} alt="EcoSLO Logo" />
         </div>
-        <button className="bg-white text-black rounded-full w-[80px] h-[39px] text-sm font-semibold">Log out</button>
+        <button className="bg-white text-black rounded-full w-[80px] h-[39px] text-sm font-avenir font-normal">
+          Log out
+        </button>
       </div>
       <div className="flex flex-grow flex-col items-center gap-[16px]">
-        <NavbarButton icon="/home.svg" label="Dashboard" link="/" />
-        <NavbarButton icon="/tree.svg" label="Trees" link="/trees" />
-        <NavbarButton icon="/volunteers.svg" label="Volunteers" link="/volunteers" />
-        <NavbarButton icon="/analytics.svg" label="Reminders" link="/reminders" />
-        <NavbarButton icon="/map.svg" label="Tree Map" link="/map" />
-        <NavbarButton icon="/settings.svg" label="Settings" link="/settings" />
+        <NavbarButton icon="/icons/home.svg" label="Dashboard" link="/" />
+        <NavbarButton icon="/icons/tree.svg" label="Trees" link="/trees" />
+        <NavbarButton icon="/icons/volunteers.svg" label="Volunteers" link="/volunteers" />
+        <NavbarButton icon="/icons/analytics.svg" label="Reminders" link="/reminders" />
+        <NavbarButton icon="/icons/map.svg" label="Tree Map" link="/map" />
+        <NavbarButton icon="/icons/settings.svg" label="Settings" link="/settings" />
       </div>
     </div>
   );
--- src/components/navbar/NavbarButton.tsx
+++ src/components/navbar/NavbarButton.tsx
@@ -7,10 +7,10 @@ interface NavbarButtonProps {
 export default function NavbarButton({ icon, label, link }: NavbarButtonProps) {
   return (
     <a href={link} className="flex flex-col items-center justify-center gap-[8px]">
-      <div className="w-[45px] h-[45px] p-[4px] flex flex-col items-center justify-center rounded-full bg-[#758656]">
-        <img src={icon} alt={label} className="w-full h-full" />
+      <div className="w-[45px] h-[45px] p-[3px] flex flex-col items-center justify-center rounded-full bg-[#758656]">
+        <img src={icon} alt={label} className="w-full h-full aspect-square" />
       </div>
-      <span className="text-base font-normal text-white h-[22px]">{label}</span>
+      <span className="text-base font-avenir font-normal text-white h-[22px]">{label}</span>
     </a>
   );
 }
