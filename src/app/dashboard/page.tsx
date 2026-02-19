95b66179c1eff7aa251945150cefd7f52f314c0c
RudyG252 <Rudyagood@gmail.com>
2026-02-19 13:41:34 -0800

    feat: put Notification Widget into dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import NotificationWidget from "@/components/NotificationWidget";
+
 export default function Dash() {
   return (
     //the whole page div//
@@ -39,7 +41,7 @@ export default function Dash() {
           {/*widgets*/}
           <div className="mt-10 grid grid-cols-[2fr_1fr_1fr] gap-8">
             {/*notifications*/}
-            <div className="h-[366px] rounded-3xl border-2 border-[#CDAA7F] bg-[#EEE0CF]"></div>
+            <NotificationWidget></NotificationWidget>
             {/*volunteers*/}
             <div className="h-[366px] rounded-3xl border-2 border-[#CDAA7F] bg-[#EEE0CF]"></div>
             {/*reminders*/}
