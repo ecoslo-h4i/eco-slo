5b0721b897f8849f96fbda9e72b55f74c0b07468
RudyG252 <Rudyagood@gmail.com>
2026-02-19 14:40:53 -0800

    feat: Create functionality for deleting notification cards

--- src/components/NotificationCard.tsx
+++ src/components/NotificationCard.tsx
@@ -3,15 +3,22 @@ import Image from "next/image";
 export default function NotificationCard({
   timestamp,
   notificationText,
+  onDelete,
 }: {
   timestamp: string;
   notificationText: string;
+  onDelete: (timestamp: string, event: React.MouseEvent<HTMLButtonElement>) => void;
 }) {
   return (
     <div className="w-auto h-[75px] pt-[12px] pl-[16px] pb-[12px] pr-[16px] rounded-lg gap-[4px] bg-[#AFC18F] mt-[10px] mb-[0px]">
       <div className="flex flex-row items-center">
         <p className="text-sm font-medium">{timestamp}</p>
-        <button className="ml-auto font-medium text-sm font-Avenir">X</button>
+        <button
+          className="ml-auto font-medium text-sm font-Avenir p-[2px] cursor-pointer"
+          onClick={(event) => onDelete(timestamp, event)}
+        >
+          X
+        </button>
       </div>
       <div className="flex flex-row items-center mt-[8px]">
         <Image src="/small_bell.png" width={24} height={25} alt="A small notification bell"></Image>
--- src/components/NotificationSidePopout.tsx
+++ src/components/NotificationSidePopout.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import NotificationCard from "./NotificationCard";
 
 export type Notification = {
@@ -10,7 +12,9 @@ export type NotificationList = {
   notificationList: Notification[];
 };
 
-export default function NotificationSidePopout(notificationList: NotificationList) {
+export default function NotificationSidePopout(notificationListProp: NotificationList) {
+  const [notificationList, setNotificationList] = useState(notificationListProp.notificationList);
+
   return (
     <div className="h-[832px] w-[503px] p-[32px] border-2 rounded-3xl gap-[16px] bg-[#F5EADD] border-[#756859] rounded-r-none">
       <h1 className="text-[40px] font-serif font-medium">Notifications</h1>
@@ -24,13 +28,15 @@ export default function NotificationSidePopout(notificationList: NotificationLis
         </button>
       </div>
       <div className="flex flex-col justify-items-center h-[605px]">
-        {mapNotifications(true, notificationList.notificationList)}
+        {mapNotifications(true, notificationList, (timestamp) =>
+          setNotificationList(notificationList.filter((notification) => notification.timestamp != timestamp)),
+        )}
       </div>
     </div>
   );
 }
 
-function mapNotifications(read: boolean, notificationList: Notification[]) {
+function mapNotifications(read: boolean, notificationList: Notification[], onDelete: (timestamp: string) => void) {
   return (
     <div className="overflow-y-hidden">
       {notificationList.map((notification) => (
@@ -38,6 +44,7 @@ function mapNotifications(read: boolean, notificationList: Notification[]) {
           notificationText={notification.notificationText}
           timestamp={notification.timestamp}
           key={notification.timestamp}
+          onDelete={onDelete}
         ></NotificationCard>
       ))}
     </div>
