3a9b127ff07951022dab36196a531c0c070c6c3b
Oliver-Cushman <olivermcushman@gmail.com>
2026-03-03 01:27:33 -0800

    feat: finish routing structure

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -4,7 +4,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
   return (
     <div className="flex min-h-screen">
       <SideNavbar />
-      <main className="flex-1">{children}</main>
+      {children}
     </div>
   );
 }
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -4,7 +4,7 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
   return (
     <div className="flex flex-col min-h-screen">
       <TopNavbar />
-      <main className="flex-1">{children}</main>
+      {children}
     </div>
   );
 }
