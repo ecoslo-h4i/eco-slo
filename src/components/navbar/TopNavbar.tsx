86993ccb396dd3d3afaa41ee4c3c3e173d548193
Oliver-Cushman <olivermcushman@gmail.com>
2026-03-03 10:16:09 -0800

    feat: complete top navbar styling

--- src/components/navbar/TopNavbar.tsx
+++ src/components/navbar/TopNavbar.tsx
@@ -1,20 +1,35 @@
+"use client";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function TopNavbar() {
+  const pathname = usePathname();
+
+  const navButton = () => {
+    return pathname === "/login" ? (
+      <a
+        href="/map"
+        className="flex flex-row items-center bg-white text-black rounded-full h-[39px] text-sm font-avenir font-normal p-3 gap-2"
+      >
+        <Image src="/icons/black-map.svg" width={18} height={18} alt="Map" />
+        <span>Back to Map</span>
+      </a>
+    ) : (
+      <a
+        href="/login"
+        className="flex items-center bg-white text-black rounded-full h-[39px] text-sm font-avenir font-normal p-3"
+      >
+        Login
+      </a>
+    );
+  };
+
   return (
     <div className="flex flex-row bg-[#758656] h-29.5 w-full pl-5 pr-5 items-center">
       <div className="w-24.5 h-24.5 flex items-center justify-space-between">
         <Image src="/icons/ecoslo-logo.png" width={98} height={98} alt="EcoSLO Logo" />
       </div>
-      <div className="flex flex-row ml-auto items-center justify-center gap-8">
-        <button className="flex items-center bg-white text-black rounded-full h-[39px] text-base font-avenir font-normal p-3">
-          Log out
-        </button>
-        <button className="flex flex-row items-center justify-center bg-white text-black rounded-full h-[39px] text-base font-avenir font-normal p-3">
-          <Image src="/icons/map.svg" width={18} height={18} alt="Map" className="stroke-black" />
-          Back to Map
-        </button>
-      </div>
+      <div className="flex flex-row ml-auto items-center justify-center gap-8">{navButton()}</div>
     </div>
   );
 }
