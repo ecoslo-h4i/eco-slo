b90991e74351c867cdef6b61aec1afe2308b8b4b
Matthew Blam <110935636+MatthewBlam@users.noreply.github.com>
2026-05-20 03:35:00 -0700

    style: update ReminderCard to match Figma Hi-Fi design

--- src/components/reminders/ReminderCard.tsx
+++ src/components/reminders/ReminderCard.tsx
@@ -29,30 +29,40 @@ export default function ReminderCard(props: ReminderCardProps) {
 
   return (
     <div
-      className={`flex w-full flex-col gap-2 rounded-2xl px-4 py-4 hover:cursor-pointer transition-colors duration-200 ${props.selected ? "bg-primary" : "bg-table-header hover:bg-active-pill"}`}
+      className={`flex w-full flex-col gap-4 rounded-3xl p-6 hover:cursor-pointer transition-colors duration-200 ${
+        props.selected
+          ? "bg-white border-l-4 border-l-primary border border-border"
+          : props.is_active
+            ? "bg-white hover:bg-active-pill"
+            : "bg-table-header hover:bg-button-muted"
+      }`}
       onClick={props.onClick}
     >
       <div className="flex flex-row items-center justify-between">
-        <span
-          className={`font-[Constantia] font-medium text-m ${props.selected ? "text-text-light" : "text-text-dark"}`}
-        >
+        <span className={`font-serif font-normal text-xl ${props.is_active ? "text-text-dark" : "text-text-subtle"}`}>
           {props.name}
         </span>
         <div
-          className={`text-sm font-avenir font-normal px-2 py-1 rounded-full ${props.selected ? "bg-primary-extra-light text-text-light" : props.is_active ? "bg-active-pill text-primary border-1 border-pill-border" : "bg-inactive-pill text-gray-500"}`}
+          className={`text-sm font-avenir font-normal px-2 py-1 rounded-full ${
+            props.is_active
+              ? "bg-active-pill text-primary border-1 border-pill-border"
+              : "bg-danger-bg text-danger border-1 border-danger-border"
+          }`}
         >
           {props.is_active ? "Active" : "Inactive"}
         </div>
       </div>
-      <span className={`text-sm font-avenir ${props.selected ? "text-text-light" : "text-text-muted"}`}>
+      <span className={`text-m font-avenir font-semibold ${props.is_active ? "text-text-muted" : "text-text-subtle"}`}>
         {props.assignees}
       </span>
       <div
-        className={`flex flex-row items-center text-sm font-avenir ${props.selected ? "text-text-light" : "text-text-muted"}`}
+        className={`flex flex-row items-center text-m font-avenir ${
+          props.is_active ? "text-text-muted" : "text-text-subtle"
+        }`}
       >
-        <Clock className="mr-1" size={16} color={`${props.selected ? "#ffffff" : "#6b6662"}`} />
+        <Clock className="mr-1" size={16} />
         <span>{scheduleSummary[0]}</span>
-        <Dot color={`${props.selected ? "#ffffff" : "#6b6662"}`} />
+        <Dot />
         <span>{scheduleSummary[1]}</span>
       </div>
     </div>
